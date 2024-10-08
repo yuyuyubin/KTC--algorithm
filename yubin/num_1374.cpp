09ae94939de89d4e1f91daa2ff7dda7f75a5ddef
yuyuyubin <129951226+yuyuyubin@users.noreply.github.com>
2024-10-08 23:00:35 +0900

    24.10.08 그리드,백트래킹
    1182,1374번 문제

--- yubin/num_1374.cpp
+++ yubin/num_1374.cpp
@@ -1,34 +1,36 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<algorithm>
 using namespace std;
 
-priority-queue<int, vector<int>, greater<int>> pq;
+priority_queue<int, vector<int>, greater<int>> pq;
 
 int main()
 {
     int N, start, end, tmax;
     cin >> N;
-    vector<pair<int,int>> class(N);
+    vector<pair<int,int>>lecture(N);
     
     for (int i = 0 ; i<N; i++){
         cin >> start >> end;
-        class[i] = {start, end};
+        lecture[i] = {start, end};
     }
 
-    sort(class.begin(), class.end());
+    sort(lecture.begin(), lecture.end());
 
     for(int j= 0; j<N; j++){
         if(pq.empty()){
-            pq.push(claas[i].second);
+            pq.push(lecture[j].second);
+        }
         else{
-            while(!pq.empty() && class[i].first >= pq.top()){
-                pq.pop()
+            while(!pq.empty() && lecture[j].first >= pq.top()){
+                pq.pop();
             }
 
         }
-        pq.push(class[i].second);
+        pq.push(lecture[j].second);
         }
-    }
-    tmax = max(tmax + int(pq.size));
+    
+    tmax = max(tmax,int(pq.size()));
 }
