f38115ac3bd857d30d299c2c3c4b3ad3dd24d94d
rungeun <wjdrmsdl5@gmail.com>
2024-10-08 21:53:46 +0900

    23/10/08 cpp

---
+++ RunGeun/CPP/n1182.cpp
@@ -0,0 +1,47 @@
+﻿#pragma GCC optimize("O3,Ofast,unroll-loops")
+#include <bits/stdc++.h>
+#if !(defined ONLINE_JUDGE || defined CODE_GROUND)
+#define TEST(X, Y) cerr << (X) << ": " << (Y) << '\n'
+#else
+#define TEST(x, Y)
+#endif
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+constexpr ll MOD = 1e9 + 7;
+constexpr int sz = 1 << 17;
+constexpr int INF = 0x7fffffff;
+int N, M;
+vector<int> nums;
+int cnt = 0;
+void rec(int depth, int sum) { 
+    if (depth== N) {
+        if (sum == M) {
+            cnt++;
+        }
+        return;
+    }
+    rec(depth + 1, sum + nums[depth]);
+    rec(depth + 1 , sum);
+}
+
+void solve() {
+    int Answer = 0;
+    cin >> N >> M;
+    nums.resize(N, 0);
+    for (int _ = 0; _ < N; ++_) {
+        cin >> nums[_];
+    }
+    rec(0, 0);
+    if(M== 0) cnt--;
+    cout << cnt;
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    solve();
+    return 0;
+}
---
+++ RunGeun/CPP/n1374.cpp
@@ -0,0 +1,60 @@
+﻿#pragma GCC optimize("O3,Ofast,unroll-loops")
+#include <bits/stdc++.h>
+#if !(defined ONLINE_JUDGE || defined CODE_GROUND)
+#define TEST(X, Y) cerr << (X) << ": " << (Y) << '\n'
+#else
+#define TEST(x, Y)
+#endif
+#define ALL(x) x.begin(), x.end()
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+constexpr ll MOD = 1e9 + 7;
+constexpr int sz = 1 << 17;
+constexpr int INF = 0x7fffffff;
+int N, M;
+int arr[8];
+bool vi[8];
+vector<pair<int, int>> vec;
+int rec(int dep) {  // 선택 개수
+    priority_queue<int, vector<int>, greater<int>> pq;
+    size_t maxt = 0;
+
+    for (int _ = 0; _ < N; ++_) {
+        if (pq.empty()) {
+            pq.push(vec[_].second);
+        } else {
+            while (vec[_].first >= pq.top()) {
+                pq.pop();
+            }
+
+            pq.push(vec[_].second);
+        }
+        maxt = max(maxt, pq.size());
+    }
+    int a= maxt;
+    return a;
+}
+
+void solve() {
+    int Answer = 0;
+    int a, b, c;
+    cin >> N;
+    for (int _ = 0; _ < N; ++_) {
+        cin >> a >> b >> c;
+        vec.push_back({b, c});
+    }
+    sort(vec.begin(), vec.end());
+    // for (int i = 0; i < vec.size(); ++i) {
+    //     cout << vec[i].first << ' ' << vec[i].second << '\n';
+    // }
+    cout << rec(0);
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    solve();
+    return 0;
+}
